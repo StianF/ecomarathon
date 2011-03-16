d76e47c50ac41ce82dcf8909716602e9724b6211
Tore Egil Halse <toreegil@gmail.com>
2011-03-16 12:20:24 +0100

    Improved simulator

--- sim.py
+++ sim.py
@@ -2,27 +2,29 @@
 
 import socket
 import sys
+import random
+import time
 
-#S_PORT = 42424		#Port used to connect to the relayserver
-#S_ADDR = "81.167.78.33"	#Server address
-#
-#s = None
-#for res in socket.getaddrinfo(C_ADDR, C_PORT, socket.AF_UNSPEC, socket.SOCK_STREAM, 0):
-#	af, socktype, proto, canonname, sa = res
-#	try:
-#		s = socket.socket(af, socktype, proto)
-#	except socket.error, msg:
-#		s = None
-#		continue
-#	try:
-#		s.connect(sa)
-#	except socket.error, msg:
-#		s.close()
-#		s = None
-#		continue
-#	break
-#if s == None:
-	#sys.exit(1)
+S_PORT = 42424		#Port used to connect to the relayserver
+S_ADDR = "81.167.78.33"	#Server address
+
+s = None
+for res in socket.getaddrinfo(S_ADDR, S_PORT, socket.AF_UNSPEC, socket.SOCK_STREAM, 0):
+	af, socktype, proto, canonname, sa = res
+	try:
+		s = socket.socket(af, socktype, proto)
+	except socket.error, msg:
+		s = None
+		continue
+	try:
+		s.connect(sa)
+	except socket.error, msg:
+		s.close()
+		s = None
+		continue
+	break
+if s == None:
+	sys.exit(1)
 
 gd = []
 fd = open("out.txt")
@@ -31,31 +33,32 @@ for line in fd:
 
 while(1):
 	for G in gd:
+		time.sleep(1)
 		data = ""
 
 		data += "V,"
-		for i in range(45):
-			data += str((rand()%20)*0.08) + ","
+		for i in range(46):
+			data += "%.2f" % ((random.random()%20)*0.08) + ","
 
-		data += "S," + str((rand()%20)*0.45)
+		data += "S," + "%.2f" % ((random.random()%20)*0.45) + ","
 
 		data += "T,"
 		for i in range(12):
-			data += str((200+rand()%300)*0.1) + ","
+			data += "%.2f" % ((200+random.random()%300)*0.1) + ","
 
 		data += "P,"
 		for i in range(3):
-			r = rand() % 100
+			r = random.random() % 100
 			if r == 0:
 				data += "err,"
 			else:
-				data += str(r) + ","
+				data += "%.2f" % (r) + ","
 
 		data += "O,"
 		for i in range(5):
-			data += str((rand()%24)) + ","
+			data += "%.2f" % ((random.random()%24)) + ","
 
 		data += "G," + G
 
-		data += "\n"
-		#s.send(data)
+		print(data)
+		s.send(data)
