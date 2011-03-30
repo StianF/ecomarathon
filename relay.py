345aa51ba0e37be321f861776246ed1eb735b41c
StianF <stian.fredrikstad@gmail.com>
2011-03-30 13:16:54 +0200

    Fixi relay

--- relay.py
+++ relay.py
@@ -18,37 +18,38 @@ for res in socket.getaddrinfo(C_ADDR, C_PORT, socket.AF_UNSPEC, socket.SOCK_STRE
 	c_srv_soc.bind(c_sa)
 	c_srv_soc.listen(1)
 	break
-	
+data = ""
 while 1:
 	car_soc, addr = c_srv_soc.accept()
 	while 1:
-		data = car_soc.recv(1024)
+		data += car_soc.recv(1024)
 		if not data:
 			print("no data")
 			break
 		else:
+			print("got some\n")
 			print(data)
 			pos = re.search("G,\d,\d,",data)
-			if not pos:
-				break
-			pos = pos.group(0).split(",",3)
-			lat  = ((int(pos[1]) >> 24) & 255) * 1.0
-			lat += ((int(pos[1]) >> 16) & 255) / 60.0
-			lat += ((int(pos[1]) >>  8) & 255) / 6000.0
-			lat += ((int(pos[1]) >>  0) & 255) / 600000.0
-			lon  = ((int(pos[2]) >> 24) & 255) * 1.0
-			lon += ((int(pos[2]) >> 16) & 255) / 60.0
-			lon += ((int(pos[2]) >>  8) & 255) / 6000.0
-			lon += ((int(pos[2]) >>  0) & 255) / 600000.0
-			data = re.sub("G,\d*,\d*","G," + str(lat) + "," + str(lon), data)
+			if pos:
+				pos = pos.group(0).split(",",3)
+				lat  = ((int(pos[1]) >> 24) & 255) * 1.0
+				lat += ((int(pos[1]) >> 16) & 255) / 60.0
+				lat += ((int(pos[1]) >>  8) & 255) / 6000.0
+				lat += ((int(pos[1]) >>  0) & 255) / 600000.0
+				lon  = ((int(pos[2]) >> 24) & 255) * 1.0
+				lon += ((int(pos[2]) >> 16) & 255) / 60.0
+				lon += ((int(pos[2]) >>  8) & 255) / 6000.0
+				lon += ((int(pos[2]) >>  0) & 255) / 600000.0
+				data = re.sub("G,\d*,\d*","G," + str(lat) + "," + str(lon), data)
 			
-		h1 = httplib.HTTPConnection(S_ADDR, S_PORT)
-		h1.request("POST", PATH, urllib.urlencode({"data": data}), {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"})
-		response = h1.getresponse()
-		if response.status != 200:
-			print("http error")
-			break
-		data = response.read()
-		h1.close
+				h1 = httplib.HTTPConnection(S_ADDR, S_PORT)
+				h1.request("POST", PATH, urllib.urlencode({"data": data}), {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"})
+				response = h1.getresponse()
+				if response.status != 200:
+					print("http error")
+					break
+				data = response.read()
+				data = ""
+				h1.close
 	car_soc.close()
 c_srv_soc.close()
