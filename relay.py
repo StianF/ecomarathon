fefd66f067db71b4e23e731e87d874102779a03b
Tore Egil Halse <toreegil@gmail.com>
2011-03-30 13:27:52 +0200

    epletre

--- relay.py
+++ relay.py
@@ -22,25 +22,26 @@ for res in socket.getaddrinfo(C_ADDR, C_PORT, socket.AF_UNSPEC, socket.SOCK_STRE
 while 1:
 	car_soc, addr = c_srv_soc.accept()
 	while 1:
-		data = car_soc.recv(1024)
+		data = ""
+		while not re.search("\n", data):
+			data += car_soc.recv(1024)
 		if not data:
 			print("no data")
 			break
 		else:
 			print(data)
 			pos = re.search("G,\d,\d,",data)
-			if not pos:
-				break
-			pos = pos.group(0).split(",",3)
-			lat  = ((int(pos[1]) >> 24) & 255) * 1.0
-			lat += ((int(pos[1]) >> 16) & 255) / 60.0
-			lat += ((int(pos[1]) >>  8) & 255) / 6000.0
-			lat += ((int(pos[1]) >>  0) & 255) / 600000.0
-			lon  = ((int(pos[2]) >> 24) & 255) * 1.0
-			lon += ((int(pos[2]) >> 16) & 255) / 60.0
-			lon += ((int(pos[2]) >>  8) & 255) / 6000.0
-			lon += ((int(pos[2]) >>  0) & 255) / 600000.0
-			data = re.sub("G,\d*,\d*","G," + str(lat) + "," + str(lon), data)
+			if pos:
+				pos = pos.group(0).split(",",3)
+				lat  = ((int(pos[1]) >> 24) & 255) * 1.0
+				lat += ((int(pos[1]) >> 16) & 255) / 60.0
+				lat += ((int(pos[1]) >>  8) & 255) / 6000.0
+				lat += ((int(pos[1]) >>  0) & 255) / 600000.0
+				lon  = ((int(pos[2]) >> 24) & 255) * 1.0
+				lon += ((int(pos[2]) >> 16) & 255) / 60.0
+				lon += ((int(pos[2]) >>  8) & 255) / 6000.0
+				lon += ((int(pos[2]) >>  0) & 255) / 600000.0
+				data = re.sub("G,\d*,\d*","G," + str(lat) + "," + str(lon), data)
 			
 		h1 = httplib.HTTPConnection(S_ADDR, S_PORT)
 		h1.request("POST", PATH, urllib.urlencode({"data": data}), {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"})
