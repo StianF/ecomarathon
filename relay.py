ba76b4dc914eafebba8b2af871eff410823a42d0
StianF <stian.fredrikstad@gmail.com>
2011-03-30 14:33:56 +0200

    jah

--- relay.py
+++ relay.py
@@ -31,27 +31,28 @@ while 1:
 		else:
 			print("got some\n")
 			print(data)
-			pos = re.search("G,\d,\d,",data)
+			pos = re.search("G,\d*,\d*,\d*,\d*,\d*,\d*,\d*,\d*",data)
 			if pos:
-				pos = pos.group(0).split(",",3)
-				lat  = ((int(pos[1]) >> 24) & 255) * 1.0
-				lat += ((int(pos[1]) >> 16) & 255) / 60.0
-				lat += ((int(pos[1]) >>  8) & 255) / 6000.0
-				lat += ((int(pos[1]) >>  0) & 255) / 600000.0
-				lon  = ((int(pos[2]) >> 24) & 255) * 1.0
-				lon += ((int(pos[2]) >> 16) & 255) / 60.0
-				lon += ((int(pos[2]) >>  8) & 255) / 6000.0
-				lon += ((int(pos[2]) >>  0) & 255) / 600000.0
-				data = re.sub("G,\d*,\d*","G," + str(lat) + "," + str(lon), data)
+				pos = pos.group(0).split(",",9)
+				lat  = int(pos[1]) * 1.0
+#				lat  = ((int(pos[1]) >> 24) & 255) * 1.0
+#				lat += ((int(pos[1]) >> 16) & 255) / 60.0
+#				lat += ((int(pos[1]) >>  8) & 255) / 6000.0
+#				lat += ((int(pos[1]) >>  0) & 255) / 600000.0
+#				lon  = ((int(pos[2]) >> 24) & 255) * 1.0
+#				lon += ((int(pos[2]) >> 16) & 255) / 60.0
+#				lon += ((int(pos[2]) >>  8) & 255) / 6000.0
+#				lon += ((int(pos[2]) >>  0) & 255) / 600000.0
+#				data = re.sub("G,\d*,\d*","G," + str(lat) + "," + str(lon), data)
 			
-				h1 = httplib.HTTPConnection(S_ADDR, S_PORT)
-				h1.request("POST", PATH, urllib.urlencode({"data": data}), {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"})
-				response = h1.getresponse()
-				if response.status != 200:
-					print("http error")
-					break
-				data = response.read()
-				data = ""
-				h1.close
+			h1 = httplib.HTTPConnection(S_ADDR, S_PORT)
+			h1.request("POST", PATH, urllib.urlencode({"data": data}), {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"})
+			response = h1.getresponse()
+			if response.status != 200:
+				print("http error")
+				break
+			data = response.read()
+			data = ""
+			h1.close
 	car_soc.close()
 c_srv_soc.close()
